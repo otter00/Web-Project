c043d8ae17c1077ef0c51ad6c2e9085fb6f06ed2
otter00 <ivlevanastia2001@mail.ru>
2023-02-08 10:42:23 +0300

    cards added (studios html/js/css)

--- JS's/studios.js
+++ JS's/studios.js
@@ -1,1 +1,229 @@
-//alert('hey');
+var json = `[{
+    "name": "Бетмен",
+    "universe": "DC Comics",
+    "alterego": "Брюс Уэйн",
+    "occupation": "борец с преступностью, филантроп, миллиардер",
+    "friends": "Робин, Бэтгерл",
+    "superpowers": "интеллект, обширные познания, знания боевых искусств, ловкость",
+    "url": "https://n1s1.hsmedia.ru/13/a5/b2/13a5b2373d5e23489d9a4949ada5b927/547x397_0xac120002_8752067681540468870.jpg",
+    "info": "По популярности человек-летучая мышь может сравниться только с Суперменом. Его образ кажется очень мистическим и мрачным: черный костюм с развевающимся плащом, устрашающий Бэтмобиль, штаб-квартира в сырой пещере. Его биография настолько же темна, как и образ. В детстве у него на глазах убили родителей, и юный Брюс поклялся всеми силами защищать родной Готэм от преступности. Брюс знаменит, несмотря на пожилой возраст: первый выпуск с его участием вышел аж в 1939 году. С каждым годом популярность только крепнет. Большую роль в этом сыграли экранизации: «Темный рыцарь», сериал «Готэм» и несколько одноименных картин. Главная причина такой любви среди фанатов — мысль, что каждый может стать Бэтменом. Если подумать, это один из немногих героев, кто добился своего положения за счет ума и упорства, а не за счет мутаций."
+},{
+    "name": "Супермен",
+    "universe": "DC Comics",
+    "alterego": "Кларк Кент",
+    "occupation": "борец за справедливость",
+    "friends": "собака Крипто",
+    "superpowers": "непробиваемость, суперсила, полёт, самоисцеление, суперзрение и суперслух, классный костюм",
+    "url" : "https://n1s1.hsmedia.ru/06/d3/73/06d37321618034ec5f2a65b09c8576e3/547x397_0xac120002_45567661540468871.jpg",
+    "info": "Полная противоположность своему противнику Бэтмену. Если Брюс Уэйн был простым человеком и стал героем, то Супермен героем родился, но вынужден был изображать выходца из бедной канзасской семьи. Последний представитель планеты Криптон был отправлен на Землю прямо перед тем, как его родина была уничтожена. Супермен — некий хамелеон от мира комиксов. В своем первом выпуске в 1938 году он был просто человек-огромный мускул, который умел только разбивать стены и выбивать двери. Позднее к его умениям добавилась способность летать. Дальше у многочисленных воплощений Кларка, от подростка в «Тайнах Смоллвиля» до величественного «Человека из стали», разный уровень и количество суперспособностей. Неизменным остается его слабое место — уязвимость к Криптониту, радиоактивному веществу с родной планеты." 
+},{
+    "name": "Железный человек",
+    "universe": "Marvel Comics",
+    "alterego": "Тони Старк",
+    "occupation": "гений, миллиардер, плейбой, филантроп",
+    "friends": "Мстители",
+    "superpowers": "высокий уровень интеллекта, широкие познания науки и техники, связь со всемирной паутиной, бронекостюмы",
+    "url" : "https://n1s1.hsmedia.ru/7b/56/08/7b5608ec3df83d872fa1162fb9e32f28/547x397_0xac120002_1773711401540468871.jpg",
+    "info": "Пожалуй, самый популярный современный супергерой. У него много общего с мрачной птицей ночи Бэтменом: оба потеряли родителей в детстве, стали борцами с преступностью без особых сверхсил, невероятно богатые. Только если Бэтменом восхищаются, то Тони Старка — обожают :) То ли дело в шикарной актерской игре Роберта Дауни мл., который выбил роль у Николаса Кейджа и Тома Круза, то ли в хорошо прописанном персонаже. Прототипом для героя послужили Илон Маск и изобретатель Говард Хьюз. Железный Человек не всегда ходил в красном модном костюме: в своих первых версиях 1963 года он был скромного серого цвета. Увы, в киновоплощении бронекостюм даже не из металла — его делали из пластика и стекловолокна."
+},{
+    "name": "Капитан Америка",
+    "universe": "Marvel Comics",
+    "alterego": "Стивен Роджерс",
+    "occupation": "супер-солдат",
+    "friends": "Мстители",
+    "superpowers": "сила, выносливость, бессмертие, быстрая регенерация, мастерство скрытности и боя",
+    "url" : "https://n1s1.hsmedia.ru/41/8f/05/418f050c767eeca8854b328914c7bccc/547x397_0xac120002_20106541761540468871.jpg",
+    "info": "Капитан Америка — персонаж, целеноправленно созданный, чтобы воплощать идеалы патриотизма. Потому он был особенно популярный во время Второй мировой войны. В комиксах обращение «Капитан Америка» применяется к любому, кто выбран правительством США. Больше всего оно закрепилось именно за Стивом Роджерсом. Стив родился в 1917 году в семье ирландских эмигрантов Сары и Джозефа Роджерсов. В детстве он был очень слабым юношей и постоянно подвергался насмешкам со стороны других ребят. Ирландские корни давали о себе знать, потому парень от драки не лез. В начале Второй мировой войны Стивен стремился попасть на фронт. Естественно, из-за здоровья ему отказывали. По воле судьбы ему предложили принять участие в секретном оборонном проекте, целью которого было создание суперсолдат. Несмотря на сложности во время эксперимента, результат был успешным. Роджерс превратился из хилого юноши в настоящего Аполлона. Актер Крис Эванс с этой ролью вполне справился:)"
+},{
+    "name": "Тор",
+    "universe": "Marvel Comics",
+    "alterego": "нет; полное имя — Тор Одинсон",
+    "occupation": "борец за справедливость, скандинавский бог",
+    "friends": "Мстители",
+    "superpowers": "всё, что может бог, плюс молот Мьелнир",
+    "url" : "https://n1s1.hsmedia.ru/52/a3/e1/52a3e14a0c8f02715b763e7a20fe1c00/547x397_0xac120002_19311926741540468872.jpg",
+    "info": "Персонаж Тора основан на образе одноименного скандинавского бога. Тор во вселенной Marvel — родной сын Одина и древнейшей богини Геи. После рождения сына Один забрал его в Асгард, где растил вместе со своей женой, богиней Фригг. Последнюю Тор всегда считал своей настоящей матерью и лишь много позже узнал правду о своем рождении. Стоит сказать, что до нынешнего Тора в исполнении Криса Хемсворта у супергероя было несколько воплощений. В одном из них он погиб в схватке с мифическим змеем Мидгардом. Это привело к Рагнареку — некоему апокалипсису в скандинавской мифологии. "
+},{
+    "name": "Чудо-женщина",
+    "universe": "DC Comics",
+    "alterego": "Диана Принс",
+    "occupation": "супергероиня, секретарь-референт",
+    "friends": "Лига Справедливости, Бэтмен, Супермен",
+    "superpowers": "сверхчеловеческая сила искорость, выносливость, полёт",
+    "url" : "https://n1s1.hsmedia.ru/87/ed/c5/87edc525d8813c7b2899e04a4e562fe9/547x397_0xac120002_6444132261540468872.jpg",
+    "info": "Пора бы разбавить эту мужскую вечеринку по-настоящему крутой девчонкой :) Принцесса-амозонка родилась на мистическом острове Темескира, расположенном в центре Бермудского треугольника, где пропадают самолеты. Своё настоящее имя Диана она получила в честь древнегреческой богини охоты, а суперспособности от богини любви Афродиты. Растили героиню в исключительно женском обществе, поэтому Чудо-женщина часто появляется в культуре как символ феминизма. У Дианы есть супер-снаряжение: лассо истины и тиара. Первое было сковано богом огня Гефестом, и оно никогда не промахивается. Человек, попавший в его петлю, неизбежно расскажет свои секреты, вспомнит забытое и даже подчинится приказам. Тиара же работает как метательное оружие, способное рассечь что угодно. Корона со звездой в центре символизирует патриотизм американской героини и служит стильным аксессуаром :)"
+},{
+    "name": "Черная вдова",
+    "universe": "Marvel Comics",
+    "alterego": "Наташа Романофф",
+    "occupation": "супергероиня, шпионка",
+    "friends": "Мстители",
+    "superpowers": "пик человеческого физического потенциала, замедленное старение, знание многих языков",
+    "url" : "https://n1s1.hsmedia.ru/e0/2b/55/e02b55b147eeaff9b3fe6bdbb36ff9ea/547x397_0xac120002_744074131540468872.jpg",
+    "info": "Супергероиня российского происхождения, родилась предположительно в 1928 году в Сталинграде. Во время Второй мировой войны она потеряла родителей и была спасена из горящего дома советским солдатом. Тот на некоторое время стал её опекуном. Повзрослев, Наташа попала в организацию «Красная Комната», где прошла многочисленные тренировки, а именно — была частью шпионской программы. Там же ей вживили сыворотку Суперсолдата, как у Капитана Америки, но в советском варианте. Благодаря ей, Наташа может использовать максимальные возможности своего организма: силу, гибкость, скорость, ловкость и т. д. Также сыворотка даёт эффект замедленного старения. По мнению фанатов, самое главное преимущество Чёрной Вдовы — то, что её сыграла красотка Скарлетт Йоханссон :) "
+}]`;
+
+document.addEventListener('DOMContentLoaded', function (e) {
+    let cards = JSON.parse(json);
+    console.log(cards);
+
+    let cardsContent = "";
+    let counter = 1; //dynamic form id
+
+    for(let card of cards) {
+        cardsContent += `<div class='gallery__card'>
+        <h2 class='card__name'>${card.name}</h2>` + 
+        `<form id='${counter}' action="#">
+        <h3>${card.universe}</h3>
+        <span class='alterego'>${card.alterego}</span> 
+        <span><p class='bold__text'>Специализация:</p> ${card.occupation}</span>
+        <span><p class='bold__text'>Союзники:</p> ${card.friends}</span>
+        <span><p class='bold__text'>Суперспособности:</p> ${card.superpowers}</span> 
+        <div><img src='${card.url}'></div>
+        <p>${card.info}</p>
+        </form>
+
+        <div class='labels' id='${counter}'>      
+        <label class="container__label" for="">
+        <input type="radio" value='1' class="container__input" name="raiting">
+            <svg class="icon">
+                <use xlink:href="../icons/sprite.svg#star-full-icon"></use>
+            </svg>
+        </label>
+
+        <label class="container__label" for="">
+        <input type="radio" value='2' class="container__input" name="raiting">
+            <svg class="icon">
+                <use xlink:href="../icons/sprite.svg#star-full-icon"></use>
+            </svg>
+        </label>
+
+        <label class="container__label" for="">
+        <input type="radio" value='3' class="container__input" name="raiting">
+            <svg class="icon">
+                <use xlink:href="../icons/sprite.svg#star-full-icon"></use>
+            </svg>
+        </label>
+
+        <label class="container__label" for="">
+        <input type="radio" value='4' class="container__input" name="raiting">
+            <svg class="icon">
+                <use xlink:href="../icons/sprite.svg#star-full-icon"></use>
+            </svg>
+        </label>
+
+        <label class="container__label" for="">
+        <input type="radio" value='5' class="container__input" name="raiting">
+            <svg class="icon">
+                <use xlink:href="../icons/sprite.svg#star-full-icon"></use>
+            </svg>
+        </label>
+        </div> 
+
+        </div>`;
+        counter++; //increment the value
+    }
+    //console.log(cardsContent);
+
+    //set cards into document
+    document.getElementById('gallery__container').innerHTML = cardsContent;
+
+    // const icons = document.querySelectorAll('.icon');
+    // console.log(icons);
+
+    // const inputs = document.querySelectorAll('.container__input')
+    // console.log(inputs);
+});
+
+
+const root = document.querySelector('#gallery__container');
+console.log(root);
+
+root.addEventListener('click', (event) => {
+    if (event.target.classList.contains('container__input')) {
+    const form = event.target.parentNode.parentNode;
+    const inputs = form.querySelectorAll('.container__input');
+    const icons = form.querySelectorAll('.icon');
+    const key = form.id;
+    const value = event.target.value;
+
+    raitingColor(icons, inputs);
+
+    // console.log(event.target.value);
+    localStorage.setItem(key, value);
+    }
+});
+
+function raitingColor(icons, inputs) {
+    icons.forEach((elem) => {
+        elem.style = 'fill: black';
+    })
+
+    for (let i = 0; i < inputs.length; i++) {
+        icons[i].style = 'fill: gold';
+        if (inputs[i].checked) {
+            break;
+        }
+    }
+}
+
+//fill the stars from localStorage (if it has them for each card)
+window.addEventListener('load', (event) => {
+    const arrLabels = document.querySelectorAll('.labels');
+
+    arrLabels.forEach((elem) => {
+        if (localStorage.getItem(elem.id)) {
+            const valueLocalStorage = localStorage.getItem(elem.id); //key: id(value)
+            console.log(valueLocalStorage);
+
+            const icons = elem.querySelectorAll('.icon');
+            for (let i = 0; i < valueLocalStorage; i++) {
+                icons[i].style = 'fill: gold';
+            }
+        }
+    })
+})
+
+//get all the keys of object (localstorage is object)
+//const allKeys = Object.keys(localStorage);
+// for (const key of allKeys) {
+//     console.log(key);
+//     console.log(localStorage[key]);
+// }
+
+
+function storageCheck() {
+if (localStorage.length != 0) {
+    const arrKeys = Object.keys(localStorage);
+    console.log('in storage we have ' + arrKeys + ' active keys');
+
+    arrKeys.forEach((key) => {
+        console.log('here we have ' + key);
+        console.log('with value ' + localStorage[key]);
+    })
+
+    // const inputs = document.querySelectorAll('.container__input');
+    // console.log(inputs);
+    
+    // const icons = document.querySelectorAll('.icon');
+    // console.log(icons);
+
+    // const form = document.querySelectorAll('form');
+    // console.log(form);
+
+    // for(let fk = 0; fk < arrKeys.length; fk++) {
+    //     for(let f = 0; f < form.length; f++) {
+    //         if (fk == f) {
+    //             console.log('equal');
+    //             //console.log(form[f].nextElementSibling.id);
+    //             //raitingColor(icons, form[f].nextElementSibling.id);
+    //         }
+    //     }
+    // }  
+}
+}
+
+//check whether localstorage has objects
+document.addEventListener('DOMContentLoaded', ()=> {
+    storageCheck();
+});
